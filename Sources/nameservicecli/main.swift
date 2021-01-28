c292f32fd917c4562696d275b0b837ff6a6b945d
Alex Tran Qui <alex@katalysis.io>
2021-01-28 23:03:47 +0100

    - remove most code for nameservicecli

--- Sources/nameservicecli/main.swift
+++ Sources/nameservicecli/main.swift
@@ -16,59 +16,11 @@ struct Nameservicecli: ParsableCommand {
             MigrateGenesisCommand.self,
             GenerateGenesisTransactionCommand.self,
             ValidateGenesisCommand.self,
-            AddGenesisAccountCommand.self,
             DebugCommand.self,
         ] + ServerContext.commands,
         defaultSubcommand: StartCommand.self
     )
 }
-
-func makeApp(
-    logger: Logger,
-    database: Database,
-    traceStore:  Writer?,
-    globalOptions: GlobalOptions
-) throws -> ABCIApplication {
-    try NameServiceApp(
-        logger: logger,
-        database: database,
-        commitMultiStoreTracer: traceStore,
-        loadLatest: true,
-        invariantCheckPeriod: globalOptions.invariantCheckPeriod
-//        BaseApp.setPruning(pruningOpts),
-//        BaseApp.setMinGasPrices(viper.GetString(server.FlagMinGasPrices)),
-//        BaseApp.setHaltHeight(viper.GetUint64(server.FlagHaltHeight)),
-//        BaseApp.setHaltTime(viper.GetUint64(server.FlagHaltTime)),
-//        BaseApp.setInterBlockCache(cache)
-    )
-}
-
-func exportApp(
-    logger: Logger,
-    database: Database,
-    traceStore: Writer?,
-    height: Int64,
-    forZeroHeight: Bool,
-    jailWhiteList: [String]
-) throws -> (JSON, [GenesisValidator]) {
-    let app = try NameServiceApp(
-        logger: logger,
-        database: database,
-        commitMultiStoreTracer: traceStore,
-        loadLatest: true,
-        invariantCheckPeriod: 1
-    )
-    
-    if height != -1 {
-        try app.load(height: height)
-    }
-    
-    return try app.exportAppStateAndValidators(
-        forZeroHeight: forZeroHeight,
-        jailWhiteList: jailWhiteList
-    )
-}
-
 let codec = NameServiceApp.makeCodec()
 
 NameServiceApp.configure()
@@ -77,10 +29,6 @@ ServerContext.defaultHome = NameServiceApp.defaultNodeHome
 InitCommand.codec = codec
 InitCommand.moduleBasicManager = NameServiceApp.moduleBasics
 InitCommand.defaultHome = NameServiceApp.defaultNodeHome
-
-
-ServerContext.makeApp = makeApp
-ServerContext.exportApp = exportApp
 
 let executor = Executor(command: Nameservicecli.self)
 executor.execute()
